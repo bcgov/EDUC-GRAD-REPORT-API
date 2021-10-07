ff294b9fb84181a4c3bcc73851f2c034b55db3a9
alexryba <alex.rybakov@gov.bc.ca>
2021-10-07 15:54:57 -0700

    Flyway for the Transcript/Certificate Rules

---
+++ api/grad/src/main/resources/db/migration/1.0/V1.0.13__DDL-DROP_TABLE-SCHOOOL_FUNDING_GROUP_CODE.sql
@@ -0,0 +1,5 @@
+--------------------------------------------------------
+--  DDL for Table SCHOOL_FUNDING_GROUP_CODE
+--------------------------------------------------------
+
+DROP TABLE SCHOOL_FUNDING_GROUP_CODE;
