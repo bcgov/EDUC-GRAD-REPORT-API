57f759ea7d44fa221a67d211b7dc4489da2f4e16
Kamal <kamal.mohammed@outlook.com>
2021-11-09 11:57:58 -0800

    Flyway Updates

--- api/isd/ISD-ReportServices-ejb/src/main/java/ca/bc/gov/educ/grad/entity/GragReportSignatureImageEntity.java
+++ api/isd/ISD-ReportServices-ejb/src/main/java/ca/bc/gov/educ/grad/entity/GragReportSignatureImageEntity.java
@@ -9,21 +9,21 @@ import java.util.UUID;
 @Data
 @Immutable
 @Entity
-@Table(name = "DIGIT_SIGNATURE")
+@Table(name = "DIGITAL_SIGNATURE")
 public class GragReportSignatureImageEntity extends BaseEntity {
 
     @Id
-    @Column(name = "DIGIT_SIGNATURE_KEY", nullable = false)
+    @Column(name = "DIGITAL_SIGNATURE_KEY", nullable = false)
     private UUID signatureId;
 
     @Lob
-    @Column(name = "DIGIT_SIGNATURE_BLOB", columnDefinition="BLOB", nullable = false)
+    @Column(name = "DIGITAL_SIGNATURE_BLOB", columnDefinition="BLOB", nullable = false)
     private byte[] signatureContent;
 
-    @Column(name = "DIGIT_SIGNATURE_CODE", nullable = false)
+    @Column(name = "DIGITAL_SIGNATURE_CODE", nullable = false)
     private String gradReportSignatureCode;
 
-    @Column(name = "DIGIT_SIGNATURE_NAME")
+    @Column(name = "DIGITAL_SIGNATURE_NAME")
     private String gradReportSignatureName;
 
 }
