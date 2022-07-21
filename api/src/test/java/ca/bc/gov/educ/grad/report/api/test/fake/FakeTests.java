73c8b2d2930b142c2b5a736da008205573d9a755
alexryba <alex.rybakov@gov.bc.ca>
2022-07-21 15:57:57 -0600

    Test coverage to 80%

---
+++ api/src/test/java/ca/bc/gov/educ/grad/report/api/test/fake/FakeTests.java
@@ -0,0 +1,35 @@
+package ca.bc.gov.educ.grad.report.api.test.fake;
+
+import ca.bc.gov.educ.grad.report.dto.impl.GradProgramImpl;
+import ca.bc.gov.educ.grad.report.dto.reports.impl.ReportServiceImpl;
+import ca.bc.gov.educ.grad.report.model.reports.*;
+import ca.bc.gov.educ.grad.report.model.transcript.ParameterPredicate;
+import ca.bc.gov.educ.grad.report.model.transcript.TranscriptTypeCode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class FakeTests {
+
+    @Test
+    public void reportServiceImplTest() {
+        ReportServiceImpl service = new ReportServiceImpl();
+        assertNotNull(service);
+        TranscriptReport transcriptReport = service.createTranscriptReport(TranscriptTypeCode.BC1950_IND, new GradProgramImpl());
+        assertNotNull(transcriptReport);
+        CertificateReport certificateReport = service.createCertificateReport();
+        assertNotNull(certificateReport);
+        GraduationReport graduationReport1 = service.createSchoolDistributionReport();
+        assertNotNull(graduationReport1);
+        GraduationReport graduationReport2 = service.createSchoolGraduationReport();
+        assertNotNull(graduationReport2);
+        NonGradReport nonGradReport = service.createNonGradReport();
+        assertNotNull(nonGradReport);
+        ParameterPredicate predicate = service.createParameterPredicate();
+        assertNotNull(predicate);
+        AchievementReport achievementReport = service.createAchievementReport();
+        assertNotNull(achievementReport);
+        PackingSlipReport packingSlipReport = service.createPackingSlipReport();
+        assertNotNull(packingSlipReport);
+    }
+}
