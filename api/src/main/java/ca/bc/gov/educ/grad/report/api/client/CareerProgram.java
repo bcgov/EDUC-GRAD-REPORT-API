97052a76cc13acaca7b89d30c5d94a02243429d8
alexryba <alex.rybakov@gov.bc.ca>
2022-09-16 14:06:52 -0600

    Remove code smells

--- api/src/main/java/ca/bc/gov/educ/grad/report/api/client/CareerProgram.java
+++ api/src/main/java/ca/bc/gov/educ/grad/report/api/client/CareerProgram.java
@@ -3,9 +3,11 @@ package ca.bc.gov.educ.grad.report.api.client;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.io.Serializable;
+
 @Data
 @EqualsAndHashCode(callSuper = false)
-public class CareerProgram {
+public class CareerProgram implements Serializable {
 
 	private String careerProgramCode;	
 	private String careerProgramName;
