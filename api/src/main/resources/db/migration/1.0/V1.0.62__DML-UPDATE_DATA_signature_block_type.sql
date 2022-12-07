1d7b231276ec03e184992e0b39faaee35cea43f3
alexryba <alex.rybakov@gov.bc.ca>
2022-12-07 13:55:10 -0700

    GRADT-435 Adult Grad Diploma updates

---
+++ api/src/main/resources/db/migration/1.0/V1.0.62__DML-UPDATE_DATA_signature_block_type.sql
@@ -0,0 +1,2 @@
+UPDATE SIGNATURE_BLOCK_TYPE SET LABEL = 'Minister of Post-Secondary' || chr(10) ||'Education and Skills' WHERE SIGNATURE_BLOCK_TYPE = 'MOAE';
+UPDATE SIGNATURE_BLOCK_TYPE SET LABEL = 'Minister of Post-Secondary' || chr(10) ||'Education and Skills' WHERE SIGNATURE_BLOCK_TYPE = 'MOAEF';
