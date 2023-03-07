8f6cd4d455af3c30247d8ef6fb6a024698af5108
alexryba <alex.rybakov@gov.bc.ca>
2023-03-07 15:03:11 -0700

    Views for school reports data

--- api/src/main/resources/db/migration/1.0/V1.0.79__DDL-CREATE_VIEW-report_school_district_vw.sql
+++ api/src/main/resources/db/migration/1.0/V1.0.79__DDL-CREATE_VIEW-report_school_district_vw.sql
@@ -1,13 +1,13 @@
 create or replace view REPORT_SCHOOL_DISTRICT_YE_VW AS
 select
-    CAST(c.GRADUATION_STUDENT_RECORD_ID AS VARCHAR2(100)) as GRADUATION_STUDENT_RECORD_ID
+    c.GRADUATION_STUDENT_RECORD_ID
 from student_certificate c
 where
     c.document_status_code='COMPL'
     and c.distribution_date is null
 union
 select
-    CAST(t.GRADUATION_STUDENT_RECORD_ID AS VARCHAR2(100)) as GRADUATION_STUDENT_RECORD_ID
+    t.GRADUATION_STUDENT_RECORD_ID
 from student_transcript t
 where
     t.document_status_code='COMPL'
@@ -21,14 +21,14 @@ where
 
 create or replace view REPORT_SCHOOL_DISTRICT_VW AS
 select
-    CAST(c.GRADUATION_STUDENT_RECORD_ID AS VARCHAR2(100)) as GRADUATION_STUDENT_RECORD_ID
+    c.GRADUATION_STUDENT_RECORD_ID
 from student_certificate c
 where
     c.document_status_code='COMPL'
     and c.distribution_date is null
 union
 select
-    CAST(t.GRADUATION_STUDENT_RECORD_ID AS VARCHAR2(100)) as GRADUATION_STUDENT_RECORD_ID
+    t.GRADUATION_STUDENT_RECORD_ID
 from student_transcript t
 where
     t.document_status_code='COMPL'
@@ -36,3 +36,5 @@ where
         select 'x' from student_certificate where GRADUATION_STUDENT_RECORD_ID = t.GRADUATION_STUDENT_RECORD_ID and document_status_code='COMPL' and distribution_date is null
     )
 ;
+
+
