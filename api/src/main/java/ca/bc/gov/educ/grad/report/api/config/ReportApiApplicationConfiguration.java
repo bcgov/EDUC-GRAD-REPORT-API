9776b151ebf9f057f368bba755b249b696966b49
alexryba <alex.rybakov@gov.bc.ca>
2023-04-26 13:51:58 -0600

    Fix date time formating

--- api/src/main/java/ca/bc/gov/educ/grad/report/api/config/ReportApiApplicationConfiguration.java
+++ api/src/main/java/ca/bc/gov/educ/grad/report/api/config/ReportApiApplicationConfiguration.java
@@ -1,15 +1,9 @@
 package ca.bc.gov.educ.grad.report.api.config;
 
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.config.EnableIntegration;
-
-import java.time.format.DateTimeFormatter;
-
-import static ca.bc.gov.educ.grad.report.api.util.ReportApiConstants.DATETIME_FORMAT;
 
 @Configuration
 @IntegrationComponentScan
@@ -26,13 +20,5 @@ public class ReportApiApplicationConfiguration {
         configurer.setIgnoreUnresolvablePlaceholders(false);
         configurer.setIgnoreResourceNotFound(false);
         return configurer;
-    }
-
-    @Bean
-    public Jackson2ObjectMapperBuilderCustomizer jacksonObjectMapperCustomization() {
-        LocalDateTimeSerializer localDateTimeSerializer = new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT));
-        return jacksonObjectMapperBuilder ->
-                jacksonObjectMapperBuilder
-                        .serializers(localDateTimeSerializer);
     }
 }
