9cb5bd0921287f195d024a73a7dcc0eca8a0df22
arybakov <alex.rybakov@cgi.com>
2023-07-04 12:28:03 -0600

    Update YE query

--- api/src/main/resources/db/migration/1.0/V1.0.88__DDL-CREATE_VIEW-report_school_district_ye_vw.sql
+++ api/src/main/resources/db/migration/1.0/V1.0.88__DDL-CREATE_VIEW-report_school_district_ye_vw.sql
@@ -3,15 +3,29 @@ select
     c.GRADUATION_STUDENT_RECORD_ID
 from student_certificate c
 where
-        c.document_status_code='COMPL'
-  and c.distribution_date is null
+    c.document_status_code='COMPL'
+    and c.distribution_date is null
 union
 select
     t.GRADUATION_STUDENT_RECORD_ID
 from student_transcript t
 where
     t.document_status_code='COMPL'
-  and t.distribution_date is null
+    and exists (
+        select 'x'
+        from student_certificate
+        where
+            GRADUATION_STUDENT_RECORD_ID = t.GRADUATION_STUDENT_RECORD_ID
+            and document_status_code = 'COMPL'
+            and distribution_date is null
+  )
+union
+select
+    t.GRADUATION_STUDENT_RECORD_ID
+from student_transcript t
+where
+    t.document_status_code='COMPL'
+    and t.distribution_date is null
 ;
 
 
