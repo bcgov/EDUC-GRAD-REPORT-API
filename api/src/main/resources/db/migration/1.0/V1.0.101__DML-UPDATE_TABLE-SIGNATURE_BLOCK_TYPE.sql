6ed782d7a0fbf806ee7c6f6022282870c7e41868
githubmamatha <harmainimamatha@gmail.com>
2024-03-21 14:34:13 -0700

    Grad2-2569 Updated the update_user value

--- api/src/main/resources/db/migration/1.0/V1.0.101__DML-UPDATE_TABLE-SIGNATURE_BLOCK_TYPE.sql
+++ api/src/main/resources/db/migration/1.0/V1.0.101__DML-UPDATE_TABLE-SIGNATURE_BLOCK_TYPE.sql
@@ -2,7 +2,7 @@ UPDATE SIGNATURE_BLOCK_TYPE
 SET
     LABEL = 'Associate Deputy Minister',
     DESCRIPTION = 'Associate Deputy Minister',
-    UPDATE_USER = 'USER',
+    UPDATE_USER = 'GRAD_REPORT_API',
     UPDATE_DATE = SYSTIMESTAMP
 WHERE
     SIGNATURE_BLOCK_TYPE = 'MOE_ADM';
