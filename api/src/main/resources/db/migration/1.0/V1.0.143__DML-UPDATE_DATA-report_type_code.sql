d93fcb515f66eb1c8500c50d427fe1f99090700c
githubmamatha <106563495+githubmamatha@users.noreply.github.com>
2025-11-12 08:44:23 -0800

    Word TRAX system has been removed from description. (#800)

---
+++ api/src/main/resources/db/migration/1.0/V1.0.143__DML-UPDATE_DATA-report_type_code.sql
@@ -0,0 +1,3 @@
+UPDATE REPORT_TYPE_CODE
+SET DESCRIPTION='A student-level report that echoes back to most current course, exam and provincial assessment activity (as well as the student''s projected or actual graduation status) that GRAD has on file for the student, as has been reported to the Ministry by schools.'
+WHERE REPORT_TYPE_CODE='ACHV';
